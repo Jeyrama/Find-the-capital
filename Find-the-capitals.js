573c2961e9a7f36108642a95eee11ed2bb82d4a6
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-09-30 22:49:16 +0200

    Update Find-the-capitals.js

--- Find-the-capitals.js
+++ Find-the-capitals.js
@@ -10,3 +10,11 @@ Example (Input --> Output):
 
 
 // Solution
+
+let capitals = function (word) {
+  let caps = [];
+	for(let i = 0; i < word.length; i++) {
+    if(word[i].toUpperCase() == word[i]) caps.push(i);
+  }
+  return caps;
+};
